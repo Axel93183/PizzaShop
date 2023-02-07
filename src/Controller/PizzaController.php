df112a204f7a31a7a90aebdee71cd9054c545bca
Axel GMX <agmx183@gmail.com>
2023-02-07 11:19:46 +0100

    Création entité Pizza

---
+++ src/Controller/PizzaController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\PizzaRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PizzaController extends AbstractController
+{
+    #[Route('/home', name: 'app_pizza_home')]
+    public function home(PizzaRepository $repository): Response
+    {
+        
+        $pizzas = $repository->findAll();
+
+
+        return $this->render('pizza/home.html.twig', [
+            'Pizzas' => $pizzas,
+        ]);
+    }
+}
